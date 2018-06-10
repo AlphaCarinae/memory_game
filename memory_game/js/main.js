98092d6ff1e6f54d136da43cf0ee144a5d483cbf
Soheil <soheil@Battleship-X.net>
2018-06-10 11:44:57 +1000

    created game logic with functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,13 +1,8 @@
 console.log("Up and running!");
 var cards = ["queen", "queen","king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
+
 
 if (cardsInPlay.length === 2) {
 
@@ -16,3 +11,23 @@ if (cardsInPlay.length === 2) {
 	}
 	else { alert("Sorry Try again!")};
 };
+
+var checkForMatch = function() {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert ("You found a match!");
+	}
+	else { alert("Sorry Try again!")};
+	};
+
+var flipcard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	if (cardsInPlay.length === 2) {
+
+	checkForMatch()
+	};
+};
+
+flipcard(0);
+flipcard(2);
